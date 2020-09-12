2020f517bccea534003b08af3dc61f0089980945
jessicayyu <jessyu2010@gmail.com>
2020-09-11 19:16:47 -0700

    Initial set-up for React

---
+++ src/Intro.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const name = "Mori";
+
+function Intro() {
+  return (
+    <div class="intro">
+      <h2 id="title">{name}</h2>
+      <h3>What is {name}?</h3>  
+      <div class="desc">
+      {name} is a Discord chat bot that links Reddit and Discord, provides moderation tools and automatic moderation actions, and utilizes open-source libraries and APIs to answer user queries regarding game and time zone information right from the chatroom.<br /><br />
+
+        It also comes with a slightly snarky personality because life's more fun that way. {name} will <em>occasionally</em> comment on whatever you're asking it to do, or say something when a command fails due to an input error.
+      </div>
+    </div>
+  );
+}
+
+export default Intro;
